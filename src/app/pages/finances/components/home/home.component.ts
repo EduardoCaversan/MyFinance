823b562614bacdf468145e8ee2e13c2841a7191a
EduardoCaversan <educaversan.dev@gmail.com>
2024-08-27 23:31:39 -0300

    #feature-001

--- src/app/pages/finances/components/home/home.component.ts
+++ src/app/pages/finances/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { DatagridComponent } from "../../../../shared/components/datagrid/datagrid.component";
+import { DatagridComponent, IDataGridColumn } from "../../../../shared/components/datagrid/datagrid.component";
 import { MovementsCardComponent } from "../../../../shared/components/movements-card/movements-card.component";
 
 @Component({
@@ -11,5 +11,20 @@ import { MovementsCardComponent } from "../../../../shared/components/movements-
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  
+  public columns: IDataGridColumn[] = [
+    { header: 'Tipo de movimentação', field: 'movementType' },
+    { header: 'Valor', field: 'value', isCurrency: true }
+  ];
+  public rows: Object[] = [
+    { movementType: 'Entrada', value: 90 },
+    { movementType: 'Saída', value: 20 },
+    { movementType: 'Entrada', value: 430 },
+    { movementType: 'Entrada', value: 290 },
+    { movementType: 'Saída', value: 1000 },
+    { movementType: 'Entrada', value: 900 },
+    { movementType: 'Saída', value: 10 },
+    { movementType: 'Saída', value: 400 },
+    { movementType: 'Saída', value: 200 },
+    { movementType: 'Entrada', value: 350 }
+  ]
 }
